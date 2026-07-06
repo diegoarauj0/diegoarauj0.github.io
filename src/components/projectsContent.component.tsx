b816187462a7819302779c0c1c0481d62b1d2e58
Diego Araujo <diegoaraujosantosbr@gmail.com>
2026-07-06 19:38:17 -0300

    adiciona o Nachtscan à lista de projetos

--- src/components/projectsContent.component.tsx
+++ src/components/projectsContent.component.tsx
@@ -13,24 +13,31 @@ const projects: InterfaceProject[] = [
   {
     name: "Mercury",
     image: "/screenshots/mercury.png",
-    description: "Encurtador de URLs com uma experiencia simples para transformar links longos em enderecos curtos.",
+    description: "Mercury e um encurtador de URLs com uma experiencia simples para transformar links longos em enderecos curtos.",
     liveUrl: "https://merc.diegoarauj0.qzz.io/",
     repositories: [{ name: "Repositorio Mercury", href: "https://github.com/diegoarauj0/mercury-web" }],
   },
   {
     name: "PocketBlog",
     image: "/screenshots/pocketblog.png",
-    description: "Blog compacto para publicar e ler posts curtos em Markdown com uma interface direta e agradavel.",
+    description: "PocketBlog e um blog para publicar e ler posts curtos em Markdown com uma interface direta e agradavel.",
     liveUrl: "https://pocketblog.diegoarauj0.qzz.io/home",
     repositories: [{ name: "Repositorio PocketBlog", href: "https://github.com/diegoarauj0/pocketblog-web" }],
   },
   {
     name: "FlatInstall",
     image: "/screenshots/flatinstall.png",
-    description: "Ferramenta para gerar script automatizado para a instalação de apps Flatpak.",
+    description: "FlatInstall e uma ferramenta para gerar script automatizado para a instalação de apps Flatpak.",
     liveUrl: "https://flatinstall.diegoarauj0.qzz.io/",
     repositories: [{ name: "Repositorio FlatInstall", href: "https://github.com/diegoarauj0/flatinstall-web" }],
   },
+  {
+    name: "NachtScan",
+    image: "/screenshots/nachtscan.png",
+    description: "NachtScan é uma plataforma web que permite verificar, em poucos segundos, se um nickname está disponível ou já está em uso em diversas plataformas na internet (redes sociais, ecossistemas de código, fóruns e jogos). ",
+    liveUrl: "nachtscan.diegoarauj0.qzz.io",
+    repositories: [{ name: "Repositorio NachtScan", href: "https://github.com/diegoarauj0/nachtscan-web" }]
+  }
 ];
 
 function ProjectContentComponent(props: InterfaceProject): React.ReactNode {
